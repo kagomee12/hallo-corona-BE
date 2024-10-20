f94b6958e937c2e64170ca5d3f355f62294c8936
kagomee12 <kalfitrah@gmail.com>
2024-10-20 23:46:34 +0700

    add all services

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,17 @@ import { ArticleModule } from './article/article.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { ClodinaryModule } from './clodinary/clodinary.module';
 import { CategoryModule } from './category/category.module';
+import { PrismaService } from './prisma.service';
+import { ArticleService } from './article/article.service';
+import { AuthService } from './auth/auth.service';
+import { ReservationService } from './reservation/reservation.service';
+import { UsersService } from './users/users.service';
+import { CloudinaryService } from './clodinary/clodinary.service';
+import { CategoryService } from './category/category.service';
 
 @Module({
   imports: [UsersModule, AuthModule, ArticleModule, ReservationModule, ClodinaryModule, CategoryModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, ArticleService, AuthService, ReservationService, UsersService, CloudinaryService, CategoryService],
 })
-export class AppModule {}
+export class AppModule { }
