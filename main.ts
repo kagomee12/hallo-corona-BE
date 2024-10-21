b20113fb520344ad718c4305f4bea4177392f36b
kagomee12 <kalfitrah@gmail.com>
2024-10-21 08:38:29 +0700

    change main.ts directory

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './src/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
+  app.enableCors();
+  await app.listen(3000); 
+}
+bootstrap();
+
+
+
