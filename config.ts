5ffab7bf650ec0db74e63abc4eae197544587cb6
kagomee12 <kalfitrah@gmail.com>
2024-10-25 17:42:35 +0700

    fix deploy

--- config.ts
+++ config.ts
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey) 
